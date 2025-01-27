51012e856d0c1da407a31228d9c4d6fc5331e7ac
Om Raja <omraja451@gmail.com>
2025-01-27 20:01:12 +0530

    add index route page complete

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path"); // inbuilt so no need to install
 let allPostsData = require("../utils/mockData");
-const {v4: uuidv4} = require("uuid");
-require("dotenv").config();
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
-
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
 app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname,"../public/images")))
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //APIs
 
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", {posts: allPostsData});
+});
 
 
-
-
-
-
-
-app.listen(process.env.PORT, ()=>{console.log(`Server running at localhost: ${process.env.PORT}`)});
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at localhost: ${PORT}`);
+});
