28c8ff5043ffb28075a78d919951c145f72b0716
Om Raja <omraja451@gmail.com>
2025-01-28 01:47:15 +0530

    fix: fixes animation of like btn when clicked , but like count stopped increasing because I have stopped page refreshing using prevent default

--- public/dom.js
+++ public/dom.js
@@ -1,7 +1,24 @@
-// let commentBtn = document.getElementsByClassName("comment-btn");
+// let likeBtns = document.getElementsByClassName("like-btn");
 
-// for(let i = 0; i < commentBtn.length; i++){
-//     commentBtn[i].addEventListener("click", ()=>{
-//         axios.get("http://localhost:3000/posts");
+// for(let btn of likeBtns){
+//     btn.addEventListener("click", ()=>{
+//         btn.classList.remove("fa-regular", "fa-heart");
+//         btn.classList.add("fa-solid", "fa-heart");
 //     });
-// };
+// }
+
+// Select all elements with the class "like-btn"
+let likeBtns = document.getElementsByClassName("like-btn");
+
+// Iterate through each button
+for (let btn of likeBtns) {
+    btn.addEventListener("click", (event) => {
+        event.preventDefault();
+        // Remove the regular heart icon classes
+        btn.classList.remove("fa-regular", "fa-heart");
+        // Add the solid heart icon classes
+        btn.classList.add("fa-solid", "fa-heart");
+
+        console.log(btn.classList);
+    });
+}
